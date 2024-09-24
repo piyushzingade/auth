90d6ea8e30cfe9228a394263b7f247eacccb6445
Piyushzingade <piyushzingade@gmail.com>
2024-09-25 00:36:44 +0530

    Added mailTrap

---
+++ backend/mailtrap/mailtrap.config.ts
@@ -0,0 +1,28 @@
+import Nodemailer from "nodemailer";
+import { MailtrapTransport } from "mailtrap";
+
+const TOKEN = "6708758534c572fad570408172d377cf";
+
+const transport = Nodemailer.createTransport(
+  MailtrapTransport({
+    token: TOKEN,
+  })
+);
+
+const sender = {
+  address: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  "piyushzingade@gmail.com",
+];
+
+transport
+  .sendMail({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
